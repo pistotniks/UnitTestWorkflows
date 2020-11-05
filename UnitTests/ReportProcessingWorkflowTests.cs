bbc203e6785504826c81ba8cd508be47a98a7bcd
SCDOM\SEPI <sepi@simcorp.com>
2020-11-05 13:00:50 +0100

    Remove methods that introduce noise

--- UnitTests/ReportProcessingWorkflowTests.cs
+++ UnitTests/ReportProcessingWorkflowTests.cs
@@ -14,62 +14,6 @@ namespace UnitTests
   {
     // Happy path: The test should pass and expectations are set correctly
     [Test]
-    public void ApproveAndResumeNoisy()
-    {
-      AutoResetEvent waitHandle = new AutoResetEvent(false);
-      WorkflowApplication wfApp = new WorkflowApplication(
-        new ReportProcessing(),
-        new Dictionary<string, object>
-        {
-          {
-            "report", new ExpenseReport
-            {
-              Employee = new Person(),
-              StartDate = DateTime.Now,
-              EndDate = DateTime.Now
-            }
-          }
-        });
-
-      ManagerResponse actualResponse = null;
-      wfApp.Completed += (wce) =>
-      {
-        actualResponse = (ManagerResponse)wce.Outputs["managerResponse"];
-        waitHandle.Set();
-      };
-      wfApp.Idle += (wce) =>
-      {
-        waitHandle.Set();
-      };
-      Exception ex = null;
-      wfApp.OnUnhandledException += arg =>
-      {
-        ex = arg.UnhandledException;
-        waitHandle.Set();
-        return UnhandledExceptionAction.Abort;
-      };
-
-      wfApp.Run();
-      waitHandle.WaitOne();
-
-      var managerResponse = new ManagerResponse {Approved = true};
-      wfApp.ResumeBookmark("SubmitResponse", managerResponse);
-
-      Retry.WaitUntil(TestContext.Progress).Execute(() => actualResponse != null);
-
-      // Assert
-      if (ex != null)
-      {
-        TestContext.Progress.WriteLine(ex);
-        throw ex;
-      }
-
-      actualResponse.Should().NotBeNull();
-      TestContext.Progress.WriteLine("Workflow completed and managers response to approve is - {0}", actualResponse.Approved.ToString());
-      actualResponse.Approved.Should().BeTrue();
-    }
-
-    [Test]
     public void ApproveAndResume()
     {
       WorkflowApplicationProxy application = new WorkflowApplicationBuilder()
@@ -98,94 +42,20 @@ namespace UnitTests
     [Test]
     public void ShouldHandleException()
     {
-      AutoResetEvent waitHandle = new AutoResetEvent(false);
-      WorkflowApplication wfApp = new WorkflowApplication(
-        new ReportProcessing(),
-        new Dictionary<string, object>
-        {
-          {
-            "report", new ExpenseReport
-            {
-              Employee = new Person(),
-              StartDate = DateTime.Now,
-              EndDate = DateTime.Now
-            }
-          }
-        });
+      WorkflowApplicationProxy application = new WorkflowApplicationBuilder()
+        .ForWorkflow(new ReportProcessing())
+        .WithData("report", new ExpenseReportBuilder().DefaultData().Build())
+        .Build();
 
-      wfApp.Idle += (wce) =>
-      {
-        waitHandle.Set();
-      };
-      Exception ex = null;
-      wfApp.OnUnhandledException += args =>
-      {
-        ex = args.UnhandledException;
-        waitHandle.Set();
-        return UnhandledExceptionAction.Abort;
-      };
-
-      wfApp.Run();
-      waitHandle.WaitOne();
+      application.Run();
 
       // Simulating a null response, where the code will throw an exception
       ManagerResponse nullData = null;
-      wfApp.ResumeBookmark("SubmitResponse", nullData);
+      application.ResumeBookmark("SubmitResponse", nullData);
 
-      Retry.WaitUntil(TestContext.Progress).Execute(() => ex != null);
+      Retry.WaitUntil(TestContext.Progress).Execute(() => application.Ex != null);
 
-      if (ex != null)
-      {
-        TestContext.Progress.WriteLine(ex);
-        throw ex;
-      }
-      Assert.Fail("Exception should have been thrown");
-    }
-
-    [Test]
-    public void ShouldHandleExceptionWithBuilder()
-    {
-      AutoResetEvent waitHandle = new AutoResetEvent(false);
-      WorkflowApplication wfApp = new WorkflowApplication(
-        new ReportProcessing(),
-        new Dictionary<string, object>
-        {
-          {
-            "report", new ExpenseReport
-            {
-              Employee = new Person(),
-              StartDate = DateTime.Now,
-              EndDate = DateTime.Now
-            }
-          }
-        });
-
-      wfApp.Idle += (wce) =>
-      {
-        waitHandle.Set();
-      };
-      Exception ex = null;
-      wfApp.OnUnhandledException += args =>
-      {
-        ex = args.UnhandledException;
-        waitHandle.Set();
-        return UnhandledExceptionAction.Abort;
-      };
-
-      wfApp.Run();
-      waitHandle.WaitOne();
-
-      // Simulating a null response, where the code will throw an exception
-      ManagerResponse nullData = null;
-      wfApp.ResumeBookmark("SubmitResponse", nullData);
-
-      Retry.WaitUntil(TestContext.Progress).Execute(() => ex != null);
-
-      if (ex != null)
-      {
-        TestContext.Progress.WriteLine(ex);
-        throw ex;
-      }
+      application.VerifyAnError();
       Assert.Fail("Exception should have been thrown");
     }
   }
