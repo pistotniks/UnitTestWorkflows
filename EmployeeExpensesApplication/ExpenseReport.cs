5f3d75dc76744d2be6dc2b47e136691ea41d8652
SCDOM\SEPI <sepi@simcorp.com>
2020-11-05 11:44:47 +0100

    Simplify UI

--- EmployeeExpensesApplication/ExpenseReport.cs
+++ EmployeeExpensesApplication/ExpenseReport.cs
@@ -8,9 +8,7 @@ namespace ExpenseReporting
   {
     [DataMember] public Person Employee { get; set; }
     [DataMember] public double Amount { get; set; }
-    [DataMember] public string Client { get; set; }
     [DataMember] public DateTime StartDate { get; set; }
     [DataMember] public DateTime EndDate { get; set; }
-    [DataMember] public string City { get; set; }
   }
 }
